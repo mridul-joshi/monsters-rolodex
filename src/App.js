29fabe4c7ee23f732ba99ddb8145c2459c71cdcc
mridul-joshi <mriduljoshimj12@gmail.com>
2021-06-20 12:07:47 +0530

    12:07

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component{
   {
     super();
     this.state={
-      monsters:[]
+      monsters:[],
+      searchField: ''
     };
   }
   componentDidMount()
@@ -18,9 +19,22 @@ class App extends Component{
     .then(users=>this.setState({monsters:users}))
   }
   render() {
+
+    const{monsters,searchField}= this.state;
+    const filteredMnsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
+
+      
     return(
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+
+        <input 
+        type='search' 
+        placeholder='Search Monsters' 
+        onChange={e=> 
+          this.setState({ searchField: e.target.value})} />      
+
+        <CardList monsters={filteredMnsters} />
           
         
       
